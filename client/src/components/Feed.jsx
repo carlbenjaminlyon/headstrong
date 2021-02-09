990ea0fe4c167cf4e41588667b37723491add3bf
David Glenn Guillory Jr <dguillory62888@gmail.com>
2021-02-09 15:54:35 -0600

    Created Like and Follow component, added linter

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -3,14 +3,13 @@ import React from 'react';
 const Feed = ({ quoteText, quoteAuthor }) => {
 
   return (
-    <div className='text wrap'>
-      <div>
-        <h1>{quoteText}</h1>
-        <h2><i>- {quoteAuthor}</i></h2>
+      <div className='text wrap'>
+          <div>
+              <h1>{quoteText}</h1>
+              <h2><i>- {quoteAuthor}</i></h2>
+          </div>
       </div>
-    </div>
   );
 };
 
 export default Feed;
-
---
+++ client/src/components/Like.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Like = () => {
+
+return (
+    <div>
+        <button>Like</button>
+    </div>
+)
+
+}
+
+ export default Like
