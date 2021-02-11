19d989f11bb9fc3f8624f651e778e09d2fdb0f9f
David Glenn Guillory Jr <dguillory62888@gmail.com>
2021-02-11 15:34:17 -0600

    Added functionality to like button, also added liked quotes feed

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'
 
 const Feed = ({ quoteText, quoteAuthor }) => {
 
@@ -6,7 +7,15 @@ const Feed = ({ quoteText, quoteAuthor }) => {
       <div className='text wrap'>
           <div>
               <h1>{quoteText}</h1>
-              <h2><i>- {quoteAuthor}</i></h2>
+              <h2 style={ { marginRight: 5 } }><i>- {quoteAuthor}</i></h2>
+              <br></br>
+              <button type='submit' onClick={ () =>{
+                    const data = { author: quoteAuthor, body: quoteText }
+                    axios.post('/quotes', data)
+                    .then(response => console.log('Quote Added', response))
+                    .catch(err => console.log('Axios Quote Error', err))
+              }
+               }>Like</button>
           </div>
       </div>
   );
