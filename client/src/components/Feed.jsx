e164d0335b9aff4513d627ff23d551d0d020780b
David Glenn Guillory Jr <dguillory62888@gmail.com>
2021-02-20 15:52:06 -0600

    Working to make styling work on live site

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -5,12 +5,12 @@ import Grid from '@material-ui/core/Grid';
 
 const Feed = ({ quoteText, quoteAuthor, entries, changePosts, username, imageURL }) => {
 
-  const time = moment().format('MMMM Do YYYY, h:mm:ss a');
+  const time = moment().calendar()
 
   return (
     <div className='text wrap'>
 
-      <div>
+      <div id='mainFeed'>
         <h1>{quoteText}</h1>
         <h2 style={ { marginRight: 5 } }><i>- {quoteAuthor}</i></h2>
         <br></br>
@@ -23,19 +23,23 @@ const Feed = ({ quoteText, quoteAuthor, entries, changePosts, username, imageURL
             .catch(err => console.log('Axios Quote Error', err));
         }
         }>Like</button>
-        <div>
-          <div id='publicPosts'>
+        </div>
+        <div id='comments'>
+
 
             <h1 >Public Posts</h1>
-          </div>
+
 
           {entries.map(entry =>
-            <div key={ entry.id } id='comments' >
-              <div>{`User:${ entry.username }`}</div>
-              <div>{`Title:${ entry.title }`}</div>
-              <div>{`Message: ${ entry.blog }`}</div>
-              {entry.journalImage ? <div ><img height='200px' width='300px' src={ entry.journalImage }/></div> : null}
-              <div>{`Posted: ${ entry.createdAt }`}<button onClick={ () => {
+            <div key={ entry.id } >
+              <p>{`${ entry.username }`}</p>
+
+              <p>{`${ entry.title }`}</p>
+              <p>{`Message: ${ entry.blog }`}</p>
+              <p>{entry.journalImage ? <img height='200px' width='300px' src={ entry.journalImage }/> : null}</p>
+              <p>{`Posted: ${time}`}</p>
+
+              <button onClick={ () => {
                 const data = {
                   friends: entry.username,
                 };
@@ -43,11 +47,11 @@ const Feed = ({ quoteText, quoteAuthor, entries, changePosts, username, imageURL
                   .then(data => console.log('Friend added'))
                   .catch(err => console.log('Error adding friend', err));
               } } style={ { marginLeft: 10 } }
-              className='btn btn-default  btn-block'>Follow</button></div>
+              className='btn btn-default  btn-block'>Follow</button>
               <hr></hr>
             </div>).sort()}
         </div>
-      </div>
+
     </div>
   );
 };
