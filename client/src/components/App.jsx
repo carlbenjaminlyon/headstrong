dbcf081d4cb8cb4b578249b5175104b68e09ef04
Ben Lyon <carlbenjaminlyon@gmail.com>
2021-12-03 11:33:53 -0600

    working modal, mood data saved and range options increased

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -15,6 +15,13 @@ import avatar from '../images/avatar.png';
 import Follow from './Follow.jsx';
 import GraphWindow from './GraphWindow.jsx';
 
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import { alpha } from '@material-ui/core/styles';
+import Typography from '@mui/material/Typography';
+
 import Astrology from './Astrology.jsx';
 
 class App extends Component {
@@ -33,6 +40,8 @@ class App extends Component {
       imageURL: null,
       username: '',
       friends: [],
+      modal: false,
+      allEntries: []
     };
     this.getFriends = this.getFriends.bind(this);
     this.getRandomQuote = this.getRandomQuote.bind(this);
@@ -46,6 +55,11 @@ class App extends Component {
     this.addProfile = this.addProfile.bind(this);
     this.getProfile = this.getProfile.bind(this);
     this.changePosts = this.changePosts.bind(this);
+    //for the modal component
+    this.handleModal = this.handleModal.bind(this);
+    //get all users journals, public or private
+    this.getAllJournals = this.getAllJournals.bind(this);
+
   }
   //change views depending on what you click
   changeView(option) {
@@ -53,6 +67,11 @@ class App extends Component {
       view: option,
     });
   }
+  //Modal click handler
+  handleModal() {
+      this.setState({modal: !this.state.modal});
+  }
+
   getAllPublicJournals() {
     axios.get('/api/journals/public')
       .then(({ data }) => {
@@ -61,6 +80,14 @@ class App extends Component {
           entries: data
         });
       }).catch(err => console.log(err));
+  }
+  //Intended to get all users mood data for graph
+  getAllJournals() {
+    axios.get('/api/journals')
+    .then(({data}) => {
+      this.setState({ allEntries: data })
+    })
+    .catch(err => console.log(err));
   }
 
   addProfile(result) {
@@ -106,7 +133,6 @@ class App extends Component {
   getRandomMemory() {
     axios.get('/api/journals')
       .then(({ data }) => {
-        console.log(data);
         const randomIndex = Math.floor(Math.random() * data.length);
         this.setState({
           memory: data[ randomIndex ],
@@ -166,6 +192,8 @@ class App extends Component {
     }
   }
   componentDidMount() {
+    this.handleModal;
+    this.getAllJournals();
     this.getFriends();
     this.getRandomQuote();
     this.getQuote();
@@ -179,13 +207,29 @@ class App extends Component {
         }))
       .catch((err) => console.warn(err));
   }
+
+
+
   logout(bool) {
     this.setState({
       login: bool
     });
   }
   render() {
-    const { login, view, roomName, imageURL, username } = this.state;
+    const { login, view, roomName, imageURL, username, modal, entries, allEntries } = this.state;
+    const { handleModal, getAllJournals } = this;
+    const style = {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      width: 600,
+      bgcolor: '#D19FE8',
+      border: '2px solid #8A2BE2',
+      boxShadow: 24,
+      borderRadius: 3,
+      p: 4,
+    };
     return (
       <div>
         {
@@ -218,7 +262,6 @@ class App extends Component {
                         HeadStrong 2.0
 
                 </div>
-                <GraphWindow />
                 <div className='nav'>
                   <Widget
                     sources={['local', 'camera', 'dropbox']} // set the sources available for uploading -> by default
@@ -295,6 +338,30 @@ class App extends Component {
                       className='Button'
                       onClick={ () => this.changeView('resource') }>Resources</Button>
                   </div>
+
+                  <div className='button'>
+                    <Button onClick={handleModal}>How're you?</Button>
+                    {modal && (
+                    <Modal
+                      aria-labelledby="transition-modal-title"
+                      aria-describedby="transition-modal-description"
+                      open={modal}
+                      onBackdropClick={handleModal}
+                      closeAfterTransition
+                      BackdropComponent={Backdrop}
+                      BackdropProps={{
+                        timeout: 500,
+                      }}
+                    >
+                      <Fade in={open}>
+                        <Box sx={style}>
+                          <GraphWindow data={entries} onLoad={allEntries}/>
+                        </Box>
+                      </Fade>
+                    </Modal>
+                    )}
+                  </div>
+
                   <div className={
                     (view === 'logout') ? 'currentButton' : 'button' }>
                     <Button
--- client/src/components/Graph.jsx
+++ client/src/components/Graph.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import {Line} from 'react-chartjs-2';
 
 
 //MUI
@@ -19,23 +20,63 @@ import {
   line,
 } from 'd3-shape';
 import { scalePoint } from 'd3-scale';
+import { render } from 'react-dom';
+import { ClassNames } from '@emotion/react';
+import axios from 'axios';
 
 
-const Graph = () => {
+const dummyData = [
+  {day: 0, mood: 10},
+  {day: 1, mood: 15},
+  {day: 2, mood: 25},
+  {day: 3, mood: 55},
+  {day: 4, mood: 60},
+  {day: 5, mood: 40},
+  {day: 6, mood: 30}
+];
 
-  const [moodData, setMoodData] = useState();
+// const Line = props => (
+//   <LineSeries.Path
+//     {...props}
+//     path={line()
+//     .x(({ arg }) => arg)
+//     .y(({ val }) => val)
+//     .curve(curveCatmullRom)}/>
+// );
+
+const Graph = ({ data, onLoad }) => {
+
+  const [userMoodData, setUserMoodData] = useState();
+  const [allMoodData, setAllMoodData] = useState();
   const [currentUser, setCurrentUser] = useState();
 
 
-  const dummyData = [
-    {day: 0, mood: 10},
-    {day: 1, mood: 15},
-    {day: 2, mood: 25},
-    {day: 3, mood: 55},
-    {day: 4, mood: 60},
-    {day: 5, mood: 40},
-    {day: 6, mood: 30}
-  ];
+
+
+  // const chart = () => {
+  //   setMoodData({
+  //     labels: ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'],
+  //     datasets: [
+  //       {
+  //         label: 'Mood',
+  //         data: [22, 45, 66, 72, 80, 55, 60],
+  //         backgroundColor: ['rgba(192, 178, 205, 0.6)'],
+  //         borderWidth: 4
+  //       }
+  //     ]
+  //   })
+  // };
+
+
+  useEffect(() => {
+    setUserMoodData(data);
+    setAllMoodData(onLoad);
+  },[])
+
+
+  const toggleView = () => {
+
+  };
 
   // const handleGraph = () => {
   //   //for each post, plot a new point on the graph from a scale of 1-100
@@ -48,11 +89,9 @@ const Graph = () => {
   // }, []);
 
   return (
-    <Paper>
-      <Typography>
-        Some text here.
-      </Typography>
-    </Paper>
+    <>
+    <Chart data={userMoodData}/>
+    </>
   );
 };
 
@@ -64,3 +103,77 @@ const Graph = () => {
 
 
 export default Graph;
+
+
+
+
+// class App extends Component {
+//   constructor(props) {
+//   this.state = {
+//     modal: false,
+//   };
+//   this.handleModal = this.handleModal.bind(this);
+// }
+
+//   handleModal() {
+//   this.setState(prevState => {
+//     return {modal: !prevState.modal}
+//     });
+//   };
+
+//   render() {
+//     const { handleModal } = this;
+//     const { modal } = this.state;
+//     return (
+//       <div>
+//         <Modal isOpen={modal} onClick={handleModal}/>
+//       </div>
+//     );
+//   }
+// }
+
+// // //---------------------------------------------
+// const Modal = ({ isOpen, onClick }) => {
+//   const [modelOpen, setModalOpen] = useState(false);
+
+//   return(
+//     <div>
+//       <Modal>
+//         <MoreStuffHere />
+//       </Modal>
+//     </div>
+//   );
+// };
+
+
+
+// //----------------------------------
+// import { useState } from "react";
+
+// function Btn({ isOpen, onClick, children }) {
+//   return (
+//     <div>
+//       <button onClick={onClick}>Clicky</button>
+//       <div className={isOpen ? "open" : "closed"}>{children}</div>
+//     </div>
+//   );
+// }
+
+// export default function App() {
+//   const [modalOpen, setModalOpen] = useState(false);
+//   const [name, setName] = useState("Billy");
+//   function handleModal() {
+//     setModalOpen(!modalOpen);
+//   }
+//   return (
+//     <div className="App">
+//       <Btn isOpen={modalOpen} onClick={handleModal}>
+//         <input
+//           type="text"
+//           value={name}
+//           onChange={(e) => setName(e.target.value)}
+//         />
+//       </Btn>
+//     </div>
+//   );
+// }
