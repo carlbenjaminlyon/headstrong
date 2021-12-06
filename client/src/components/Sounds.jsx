bff03d41f35adf7765025fba53a5d118a49420f3
t-suarez <tsuare1@gmail.com>
2021-12-06 02:27:18 -0600

    finished feature

--- client/src/components/Sounds.jsx
+++ client/src/components/Sounds.jsx
@@ -32,6 +32,17 @@ const Sounds = ({ data, onLoad }) => {
       'release' : 0,
     },
   }).toDestination();
+
+  const pad = new Tone.PolySynth({
+    'volume': -25,
+    'envelope': {
+      'attack': 10,
+      'decay': 0.1,
+      'release': 2
+    },
+    'activeVoices': 3,
+  }).toDestination();
+
   const amSynth = new Tone.AMSynth({
     options: {
       harmonicity: 0.5
@@ -42,9 +53,14 @@ const Sounds = ({ data, onLoad }) => {
   const pingPong = new Tone.PingPongDelay("8d", 0.3).toDestination();
   const autoPanner = new Tone.AutoPanner("8n").toDestination().start();
   const reverb = new Tone.Reverb(10).toDestination();
+  const stereoWidener = new Tone.StereoWidener(1);
+  const chorus = new Tone.Chorus(4, 2.5, 0.5).toDestination().start();
   synth.connect(pingPong);
   synth.connect(autoPanner);
   synth.connect(reverb);
+  pad.connect(reverb);
+  pad.connect(stereoWidener);
+  pad.connect(chorus);
 
   const bass = new Tone.MonoSynth({
     'volume' : -15,
@@ -62,27 +78,23 @@ const Sounds = ({ data, onLoad }) => {
     }
   }).toDestination();
 
-
-//   // repeated event every 8th note
-// Tone.Transport.scheduleRepeat((time) => {
-// 	// use the callback time to schedule events
-// 	osc528.start(time).stop(time + 0.1);
-// }, "8n");
-// // transport must be started before it starts invoking events
-
-// Tone.Transport.scheduleRepeat((time) => {
-//   console.log(amSynth.get());
-// }, "4n");
-
 const seq = new Tone.Sequence((time, note) => {
 	bass.triggerAttackRelease(note, "1n", time);
 	// subdivisions are given as subarrays
 }, ["B1", "A1", "G1", "F#1", "D1", "B1", "E1", "A1"], "1n").start(0);
 
-// const seq2 = new Tone.Sequence((time, note) => {
-// 	synth.triggerAttackRelease(note, "16n", time);
-// 	// subdivisions are given as subarrays
-// }, [["F#4", "A4"], ["B4", "A4"], ["G4", "B4"], ["E4", "G4"]], "1n").start(0);
+const padPart = new Tone.Loop(function(time){
+  pad.triggerAttackRelease(['B5', 'D5', 'F#5'], '1n', time);
+  pad.triggerAttackRelease(['A5', 'C#5', 'E5'], '1n', time + 2.792);
+  pad.triggerAttackRelease(['G5', 'B5', 'D5'], '1n', time + 5.584);
+  pad.triggerAttackRelease(['F#5', 'A5', 'C#5'], '1n', time + 8.376);
+  pad.triggerAttackRelease(['D5', 'F#5', 'A5'], '1n', time + 11.168);
+  pad.triggerAttackRelease(['B5', 'D5', 'F#5'], '1n', time + 13.960);
+  pad.triggerAttackRelease(['E5', 'G5', 'B5'], '1n', time + 16.752);
+  pad.triggerAttackRelease(['A5', 'C#5', 'E5'], '1n', time + 19.544);
+}, 22.336);
+
+const hihat = new Tone.MetalSynth();
 
 const snare = new Tone.NoiseSynth({
 	'volume' : -20,
@@ -98,10 +110,6 @@ const snare = new Tone.NoiseSynth({
 	}
 }).toDestination();
 
-// const snarePart = new Tone.Loop(function(time){
-// 	snare.triggerAttack(time);
-// }, '4n').start('8n');
-
 const kick = new Tone.MembraneSynth({
 	'envelope' : {
 		'sustain' : 0,
@@ -111,11 +119,9 @@ const kick = new Tone.MembraneSynth({
 	'octaves' : 10
 }).toDestination();
 
-// const kickPart = new Tone.Loop(function(time){
-// 	kick.triggerAttackRelease('D1', '8n', time);
-//   kick.triggerAttackRelease('D1', '16n', time + 0.174);
-//   kick.triggerAttackRelease('D1', '16n', time + 0.870);
-// }, '2n').start(0);
+const hihatPart = new Tone.Loop(function(time){
+  hihat.triggerAttackRelease('D5', '16n', time);
+}, '16n');
 
 const kickPart = new Tone.Loop(function(time){
   kick.triggerAttackRelease('D1', '8n', time);
@@ -139,21 +145,10 @@ return (
           // subdivisions are given as subarrays
         }, generatedSequence, "1n").start(0);
 
-        // synth.triggerAttackRelease("C4", "8n");
-        // synth.triggerAttackRelease("E4", "8n");
-        // synth.triggerAttackRelease("G4", "8n");
-        // synth.toFrequency("4n");
-        // synth.start;
-        // amSynth.triggerAttackRelease(528, "8n").connect(autoPanner);
         console.log('this is hash now: ', hash);
         console.log(generateSequenceFrom(hash, 0, []));
         Tone.Transport.start();
 
-        // osc432.start();
-        // osc528.start();
-        // console.log(osc432.get());
-        // osc432.stop();
-        // console.log(osc432.get());
       }
     }>Play</Button>
     <Button variant='contained' onClick={() => {
@@ -164,11 +159,15 @@ return (
     <Button variant='contained' onClick={() => {
       kickPart.start(0);
       snarePart.start('8n');
+      padPart.start(0);
+      hihatPart.start(0);
     }
   }>Sick</Button>
   <Button variant='contained' onClick={() => {
       kickPart.stop();
       snarePart.stop();
+      padPart.stop();
+      hihatPart.stop();
     }
   }>Chill</Button>
   </div>
